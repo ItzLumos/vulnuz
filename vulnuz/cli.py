3cd5878691ba1c4aaaf79fde557b728e6929b899
ItzLumos <ffztaxits@gmail.com>
2026-04-05 16:14:01 +0100

    feat: polished header_analyzer.py and coded cli.py

--- vulnuz/cli.py
+++ vulnuz/cli.py
@@ -13,11 +13,14 @@ def main():
             break
     
         if args[0] == "analyze-headers":
-            if len(args) < 2:
-                print("Usage: analyze-headers <url>")
+            if len(args) < 3 or "-u" not in args:
+                print("Usage: analyze-headers -u <url> -f <file-path>")
                 continue
 
-            headers = http_probe(args[1])
+            url_index = args.index("-u") + 1
+            url = args[url_index]
+
+            headers = http_probe(url)
 
             if headers == -1:
                 print("ERROR: Could not resolve/connect to the given URL")
@@ -25,13 +28,66 @@ def main():
 
             findings = analyze_headers(headers)
 
+            if "-f" in args:
+                filepath_index = args.index("-f") + 1
+                filepath = args[filepath_index]
+
+                with open(filepath, "w") as f:
+                    for finding in findings:
+                        f.write(f"{finding}\n")
+
+                    f.close()
+
+                print(f"Header analysis registered at: '{filepath}'")
+
+                continue
+
             for finding in findings:
                 print(finding)
         
+        elif args[0] == "http-probe":
+            if len(args) < 3 or "-u" not in args:
+                print("Usage: http-probe -u <url> -f <file-path>")
+                continue
+
+            url_index = args.index("-u") + 1
+            url = args[url_index]
+
+            headers = http_probe(url)
+
+            if headers == -1:
+                print("ERROR: Could not resolve/connect to the given URL")
+                continue
+
+            if "-f" in args:
+                filepath_index = args.index("-f") + 1
+                filepath = args[filepath_index]
+
+                with open(filepath, "w") as f:
+                    f.write(str(headers))
+
+                    f.close()
+                
+                print(f"Headers registered at {filepath}")
+                
+                continue
+
+            print(headers)
+
         else:
             print(f'ERROR: "{cmd}" is not a command/external module')
             continue
 
 
 if __name__ == "__main__":
+    banner = """
+██╗   ██╗██╗   ██╗██╗     ███╗   ██╗██╗   ██╗███████╗
+██║   ██║██║   ██║██║     ████╗  ██║██║   ██║╚══███╔╝
+██║   ██║██║   ██║██║     ██╔██╗ ██║██║   ██║  ███╔╝ 
+╚██╗ ██╔╝██║   ██║██║     ██║╚██╗██║██║   ██║ ███╔╝  
+ ╚████╔╝ ╚██████╔╝███████╗██║ ╚████║╚██████╔╝███████╗
+  ╚═══╝   ╚═════╝ ╚══════╝╚═╝  ╚═══╝ ╚═════╝ ╚══════╝
+made by: ItzLumos (GitHub)
+"""
+    print(banner)
     main()
